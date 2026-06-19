0165b204b78942ed733ee81f7a08f7d56d9bbf12
Matheus Lima da Silva <f592976@corp.caixa.gov.br>
2026-06-19 15:47:13 -0300

    Evoluções até sexta-feira na Caixa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,56 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
+// components
+import { NewComponent } from './components/new-component/new-component.component';
+import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
+import { DscHeaderComponent } from 'sidsc-components/dsc-header';
+import { DscButtonHeaderComponent } from 'sidsc-components/dsc-button-header';
+import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  standalone: true,
+  imports: 
+    [CommonModule, RouterOutlet, NewComponent,
+    TemplateBindingComponent, DscHeaderComponent,
+    DscButtonHeaderComponent, TemplateVariablesComponent],
+  styles: [
+    `
+    h1 {
+      color: blue;
+    }
+    `
+  ],
+  template: `
+  <!-- <router-outlet></router-outlet> -->
+   <dsc-header toggleButton="true" [acronym] = '"Meu projeto Angular"' 
+   accessModifier='externo' toggleButtonLabel="true"
+   additionalInfo="Olá", userName="Matheus Lima"
+   userWorkUnit="Desenvolvedor Front End" >
+    <dsc-button-header icon="person" iconStyle="rounded"
+    label="perfil" href="/perfil">
+    </dsc-button-header>
+  </dsc-header>
+
+
+  <h1>Curso de Angular</h1>
+  <hr/>
+  <br/>
+
+
+  <app-template-binding/>
+  <!-- <div class="theme-dark">
+    <app-new-component/>
+  </div> -->
+
+  <app-template-variables/>
+  `,
 })
 export class AppComponent {
-  title = 'test-project';
+  title = 'Curso de Angular';
 }
+
+//<router-outlet/>
+
